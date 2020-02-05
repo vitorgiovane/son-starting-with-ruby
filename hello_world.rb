18fcc933ed638ff00b5a30cfc4b5755dd2920995
Vitor Giovane <vitor.alves@pontomais.com.br>
2020-02-05 13:41:04 -0300

    cria um Hello World

---
+++ hello_world.rb
@@ -0,0 +1,11 @@
+# puts "Hello, World!"
+# print "Hello, World!"
+# printf("Hello, World")
+
+puts "Whats is your name?"
+$name = STDIN.gets
+puts "Your name is " + $name + " from Github"
+
+# var // local
+# @var // instância
+# $var // global
