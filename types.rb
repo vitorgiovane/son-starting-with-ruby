8499986922f4d313960d70650453a13bf7670fb6
Vitor Giovane <vitor.alves@pontomais.com.br>
2020-02-24 13:09:37 -0300

    finaliza aula de tipos

---
+++ types.rb
@@ -0,0 +1,26 @@
+# my_string = String.new('This is a test from String class')
+
+# puts my_string.reverse!
+
+# range = (2..4).to_a
+
+# puts range.inspect
+
+# teachers = Array.new(3)
+
+# teachers[0] = 'L'
+# teachers[1] = 'LC'
+# teachers[2] = 'W'
+
+# teachers = Array.[]('L', 'LC', 'W')
+# teachers2 = Array.[]('E', 'H', 'G')
+
+# teachers.concat(teachers2)
+
+# teachers.clear
+
+# puts teachers.length
+
+people = Hash["Leonan" => 10, "Luiz" => 20, "Wesley" => 30]
+
+puts "#{people['Leonan']}"
